4665adce2a6a1c9fb9da02baf45c0ffd6f45d0e1
TheDemonDante <31363773+TheDemonDante@users.noreply.github.com>
2021-01-13 22:55:39 +0530

    Finally

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('keydown', function(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+})
+
+document.querySelectorAll('.key').forEach(key => key.addEventListener('transitionend', function() {
+    this.classList.remove('playing');
+}))
